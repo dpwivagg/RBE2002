3cc4d92e84cfde215b9e6e7f127979023987d6dc
kagura <jvahe@live.com>
2017-04-25 17:37:21 -0400

    bens code

--- src/FlameSense.cpp
+++ src/FlameSense.cpp
@@ -10,8 +10,6 @@ int count=0;
 int countLess50 = 0;
 int countLess20 = 0;
 int countLess100 = 0;
-int countLess500 = 0;
-int countLess800 = 0;
 int lowest = 5000;
 
 void FlameSense::init() {
@@ -19,85 +17,92 @@ void FlameSense::init() {
     servoTilt.attach(servoTiltDigitalPin, 800, 2200);
 }
 
-double FlameSense::get(bool close) {
-    if(lowest < 800) return true;
-    return false;
-    // return close?radiusClose:radiusFar;
-}
+
 
 double FlameSense::getTurn() {
     return lowestTurn - 90;
 }
 
-bool FlameSense::update() {
-
+bool FlameSense::updateClose() {
     if(tilt<=110){
-        if(turn<100) {
+        if(turn<=110) {
 
-            if(turn %5 == 0){
+            if(turn %2 == 0){
 
                 if(sampleNumber < 10){ // goes through so man times to take readings
                     count += analogRead(A0);
                     sampleNumber++;
                 } else {
-
-
                     if (count/10<=lowest){//sets position and number of lowest read
                       lowest = count/10;
                       lowestTurn = turn - turnError;
                       lowestTilt = tilt - tiltError;
                     }//end if count<lowest
-                    if(count/10<=800){
-                        countLess800++;
-                        if(count/10<=500){
-                            countLess500++;
-                            if(count/10<=100){
-                                countLess100++;
+                    if(count/10<=100){
+                        countLess100++;
+                        if(count/10<=50){
+                            countLess50++;
+                            if(count/10<=20){
+                                countLess20++;
                             }
                         }
                     }
-
                     sampleNumber = 0;
                     count = 0;
                     turn++;
-                    // turn+=5;
-
                 }
-
-
             } else {//end of if turn % 5
                 turn++;
-                // turn+=5;
             }
         } else {
-            turn  = 0;
+            turn  = 70;
             tilt += 5;
         }
 
     } else {
-        tilt = 60;
+        tilt = 50;
+        turn  = 70;
+        radiusClose = GuessRadClose(countLess100,countLess50,countLess20);
+        countLess20 = 0;
+        countLess50 = 0;
+        countLess100 = 0;
     }
-
-    radiusClose = GuessRadClose(countLess100,countLess50,countLess20);
-    radiusFar = GuessRadFar(countLess800,countLess500,countLess100);
-
     servoTurn.write(turn);
     servoTilt.write(tilt);
-
+    return count > 0;
+}
+bool FlameSense::updateFar() {
+        if(turn<=110) {
+            if(turn %5 == 0){
+                if(sampleNumber < 5){ // goes through so man times to take readings
+                    count += analogRead(A0);
+                    sampleNumber++;
+                } else {
+                    if (count/5<=lowest){//sets position and number of lowest read
+                      lowest = count/5;
+                      lowestTurn = turn - turnError;
+                      lowestTilt = tilt - tiltError;
+                    }//end if count<lowest
+                    sampleNumber = 0;
+                    count = 0;
+                    turn++;
+                }
+            } else {//end of if turn % 5
+                turn++;
+            }
+        } else {
+            turn  = 0;
+        }
+    servoTurn.write(turn);
     return count > 0;
 }
 
-double FlameSense::GuessRadFar(int less8,int less5, int less1){
-  if(less8>1){
-    if(less5>10){
-      if(less1>8){
-        return(10);
-      }
-      return(15);
-    }
-    return(20);
-  }
-  return(25);
+void setTurn(int t){
+  servoTurn.write(turn);
+}
+
+void setTilt(int t){
+  servoTilt.write(t);
 }
 
 double FlameSense::GuessRadClose(int less100,int less50, int less20){
@@ -136,4 +141,25 @@ double FlameSense::getZ(int angle, int radius, int height){
     return(9.5);
    }
    return(h);
+}
+bool FlameSense::putOutFlame(){
+  servoTurn.write(lowestTurn);
+  servoTilt.write(lowestTilt);
+  fanState() = true
+  if (read>850){
+    return true;
+  }
+  fanState() = false
+  return false;
+}
+bool FlameSense::fanState(){
+  return fanOn;
+}
+
+bool isItLit(){
+    if (isItLit){
+      isItLit = !putOutFlame();
+    }
+    return isItLit;
+
 }
